0019613fc4171294a6d802acc0ee549758416cf9
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-08 18:53:25 +0100

    commit

---
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/ResourceApiGatewayCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.resource.ResourceAPIGatewayCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceApiGatewayCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceApiGatewayCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/ResourceApiGatewayCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.resource.ResourceAPIGatewayCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceApiGatewayCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
