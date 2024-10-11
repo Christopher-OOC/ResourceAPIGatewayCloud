08190cc1e5a0c6f42f6375f08a3565a0097922fd
Christopher-OOC <olojedechristopher24@gmail.com>
2024-10-11 19:53:53 +0100

    ciommit

--- src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/ResourceApiGatewayCloudApplication.java
+++ src/main/java/com/example/keycloak/resource/ResourceAPIGatewayCloud/ResourceApiGatewayCloudApplication.java
@@ -11,9 +11,7 @@ public class ResourceApiGatewayCloudApplication {
 	// Eureka Client
 	// Eureka Client
 	// Eureka Client
-	// Eureka Client
-	// Eureka Client
-
+	// Eureka Clie
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceApiGatewayCloudApplication.class, args);
 	}
